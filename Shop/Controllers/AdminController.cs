1d3c7190c320873809b6125797ec6b548fc3e3f2
asawicki96 <asawicki1996@gmail.com>
2020-08-23 21:20:44 +0200

    Edit account

--- Shop/Controllers/AdminController.cs
+++ Shop/Controllers/AdminController.cs
@@ -7,11 +7,12 @@ using Shop.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 
+
 namespace Shop.Controllers
 {
     public class AdminController : Controller
     {
-        private UserManager<IdentityUser>  UserMgr { get; }
+        private UserManager<IdentityUser> UserMgr { get; }
         private SignInManager<IdentityUser> SignInMgr { get; }
 
         public AdminController(UserManager<IdentityUser> userManager,
@@ -26,7 +27,7 @@ namespace Shop.Controllers
         public IActionResult Login(string returnUrl)
         {
             ViewBag.ReturnUrl = returnUrl;
-            
+
             if (!User.Identity.IsAuthenticated)
             {
                 return View();
@@ -35,10 +36,10 @@ namespace Shop.Controllers
             {
                 return RedirectToAction("Dashboard", "Admin");
             }
-            
+
         }
 
-       
+
         public async Task<IActionResult> Logout()
         {
             if (User.Identity.IsAuthenticated)
@@ -53,14 +54,14 @@ namespace Shop.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View(model);
             }
             else
             {
                 var result = await SignInMgr.PasswordSignInAsync(model.Username, model.Password, false, false);
-                if(result.Succeeded)
+                if (result.Succeeded)
                 {
                     return RedirectToAction("Dashboard", "Admin");
                 }
@@ -69,10 +70,69 @@ namespace Shop.Controllers
             }
         }
 
+
+
         [Authorize(Roles = "Administrator")]
         public IActionResult Dashboard()
         {
             return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Edit()
+        {
+
+            var user = await UserMgr.GetUserAsync(HttpContext.User);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [Authorize(Roles ="Administrator")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string UserName, string password, string old_password)
+        {
+            var user = await UserMgr.GetUserAsync(HttpContext.User);
+            if (user != null)
+            {
+                
+                if (!string.IsNullOrEmpty(UserName))
+                    user.UserName = UserName;
+                else
+                    ModelState.AddModelError("", "Username cannot be empty");
+
+                if (!string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(old_password))
+                {
+                    IdentityResult result = await UserMgr.ChangePasswordAsync(user, old_password, password);
+
+                    if (result.Succeeded)
+                        return RedirectToAction("Dashboard");
+                    else
+                        ViewBag.error = "Old passord is incorrect";
+                        return View(user);
+                }
+
+                if (!string.IsNullOrEmpty(UserName))
+                {
+                    IdentityResult result = await UserMgr.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Dashboard");
+                    else
+                        return View(user);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+                return View(user);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
         }
     }
 }
